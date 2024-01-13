a05da7801e5ded19f5a03c189d276d360d67b418
Tomi <Tomi@POTATO-PC>
2024-01-13 17:05:53 +0100

    Add project files.

---
+++ Student-Backend/Models/StudentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Student_Backend.Models;
+
+namespace ReactAspCrud.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Student { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("SERVER=localhost;PORT=3306;DATABASE=lbs;USER=root;PASSWORD=;SSL MODE=none;");
+        }
+    }
+}
